357922de54e898a95138287479a5736aaa9645b9
kowalix007 <kowalix007@gmail.com>
2019-12-19 11:42:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -19,6 +19,7 @@ def age_appropriate_birthday(birthday_kids)
 birthday_kids.each do |kids_name, age|
 if kids_name < 13
 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+else 
 end
 end
 end
